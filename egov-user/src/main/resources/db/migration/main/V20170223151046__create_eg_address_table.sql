sivaprakash123/egov-services

WIP Get user by id flow working.

core/egov-user/src/main/resources/db/migration/main/V20170223151046__create_eg_address_table.sql
@@ -1,0 +1,17 @@
+CREATE TABLE eg_address (
+    house_no_bldg_apt character varying(32),
+    sub_district character varying(100),
+    post_office character varying(100),
+    landmark character varying(256),
+    country character varying(50),
+    user_id bigint not null references eg_user(id),
+    type character varying(50),
+    street_road_line character varying(256),
+    city_town_village character varying(256),
+    area_locality_sector character varying(256),
+    district character varying(100),
+    state character varying(100),
+    pin_code character varying(10),
+    id serial NOT NULL primary key,
+    version bigint DEFAULT 0
+);

core/egov-user/src/test/resources/sql/clearUserRoles.sql
@@ -1,0 +1,1 @@
+DELETE FROM eg_user_role;

eyrdworkspace/core-services

egov-user/src/main/resources/db/migration/main/V20170223151046__create_eg_address_table.sql
@@ -1,0 +1,17 @@
+CREATE TABLE eg_address (
+    housenobldgapt character varying(32),
+    subdistrict character varying(100),
+    postoffice character varying(100),
+    landmark character varying(256),
+    country character varying(50),
+    userid bigint not null references eg_user(id),
+    type character varying(50),
+    streetroadline character varying(256),
+    citytownvillage character varying(256),
+    arealocalitysector character varying(256),
+    district character varying(100),
+    state character varying(100),
+    pincode character varying(10),
+    id serial NOT NULL primary key,
+    version bigint DEFAULT 0
+);

egov-user/src/test/resources/sql/clearUserRoles.sql
@@ -1,0 +1,1 @@
+DELETE FROM eg_userrole;